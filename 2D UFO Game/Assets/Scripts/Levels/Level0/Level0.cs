d527976439bb1833a832deb12aea60c16124f76e
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-10 22:53:09 +0100

    loading scene working, total is not static

---
+++ 2D UFO Game/Assets/Scripts/Levels/Level0/Level0.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level0 : MonoBehaviour
+{
+    public GameObject BlackHole;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")){
+            BlackHole.SetActive(true);
+        }
+    }
+}
---
+++ 2D UFO Game/Assets/Scripts/LoadSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
