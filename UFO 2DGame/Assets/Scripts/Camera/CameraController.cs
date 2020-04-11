57ff9b04af28242008f954e0293681ed5600dc69
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-11 17:34:12 +0100

    Cameracontroller.cs

--- UFO 2DGame/Assets/Scripts/Camera/CameraController.cs
+++ UFO 2DGame/Assets/Scripts/Camera/CameraController.cs
@@ -6,17 +6,17 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
 
-    private Vector3 offset;
+    private float offsetY;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offsetY = transform.position.y - player.transform.position.y;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = new Vector3(0, player.transform.position.y + offsetY, 0);
     }
 }
