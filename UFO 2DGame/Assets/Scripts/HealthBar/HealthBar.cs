6708ad99683ce515f829cdfb6205b12ac9358c5b
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-11 23:47:12 +0100

    HealthBar

---
+++ UFO 2DGame/Assets/Scripts/HealthBar/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Slider))]
+public class HealthBar : MonoBehaviour
+{
+    private Slider slider;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void SetMaxHealth(float maxHealth)
+    {
+        slider.maxValue = maxHealth;
+
+        SetHealth(maxHealth);
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+}
