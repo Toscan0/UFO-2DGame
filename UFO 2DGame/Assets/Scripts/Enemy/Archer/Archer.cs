077038f3c58edf5debb96481edb368c080a97f5b
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-12 19:37:48 +0100

    archer changed

--- UFO 2DGame/Assets/Scripts/Enemy/Archer/Archer.cs
+++ UFO 2DGame/Assets/Scripts/Enemy/Archer/Archer.cs
@@ -2,7 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Archer : Enemy
 {
-   
+    [SerializeField]
+    private float moveSpeed = 5;
+
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        //Shoot once every frame because fuck yeah!!
+        Shoot();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+    private void Shoot()
+    {
+        //Debug.Log("I shooted!");
+    }
 }
