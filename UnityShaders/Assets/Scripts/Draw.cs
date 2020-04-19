02ecce9dbab01d4f39043fab123def32b12addfe
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-19 19:47:44 +0100

    Two shaded color

---
+++ UnityShaders/Assets/Scripts/Draw.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+    #region PRIVATE_VARIABLES
+
+
+    [SerializeField]
+    private int numberOfVertices = 400;
+    [SerializeField]
+    private float radius = 0.2f;
+
+    private float lineWidthMultiplier = 1f;
+    private float lineStartWidth = 0.02f;
+    private float lineEndWidth = 0.02f;
+
+    private LineRenderer lineRenderer;
+    private Renderer holoRenderer;
+
+    #endregion
+
+    void Awake()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+
+        lineRenderer.positionCount = numberOfVertices;
+        lineRenderer.useWorldSpace = false;
+
+        lineRenderer.widthMultiplier = lineWidthMultiplier;
+        lineRenderer.startWidth = lineStartWidth;
+        lineRenderer.endWidth = lineEndWidth;
+
+
+        holoRenderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawCircle();
+    }
+
+    public void DrawCircle()
+    {
+        lineRenderer.loop = true;
+
+        
+
+        // One more point so the last one be equal to the first one
+        lineRenderer.positionCount = numberOfVertices + 1;
+
+        float x;
+        float y;
+        float z = 0;
+
+        float angle = 0f;
+
+        for (int i = 0; i < (numberOfVertices + 1); i++)
+        {
+            //x = (float) (Math.Sin((Math.PI / 180) * angle) * radius);
+            //y = (float) (Math.Cos((Math.PI / 180) * angle) * radius);
+
+            x = (float)(radius * (1 - Math.Cos((Math.PI / 180) * angle)));
+            y = (float)(radius * (Math.Sin((Math.PI / 180) * angle)));
+
+            lineRenderer.SetPosition(i, new Vector3(x, y, z)); //new Vector3(y, z, x)
+
+            angle += (360f / numberOfVertices);
+
+            //Debug.Log("Pos_" + i + " ( " + x + "; " + y + "; " + z + ")");
+        }
+
+        //bigger then the ampoule
+        if (radius > 0.155)
+        {
+            holoRenderer.material.SetFloat("_IsSecondColorOn", 1f);
+        }
+        else
+        {
+            holoRenderer.material.SetFloat("_IsSecondColorOn", 0f);
+        }
+    }
+}
---
+++ UnityShaders/Assets/Shaders/TwoColorshader.shader
@@ -0,0 +1,66 @@
+﻿Shader "Unlit/TwoColorshader"
+{
+    Properties
+    {
+        //_MainTex ("Texture", 2D) = "white" {}
+		_DefaultColor ("Default Color", color) = (0, 0, 1, 1)
+		_SecondColor ("Second Color", color) = (1, 0, 0, 1)
+		// shaders dont support bool -> false = 0, true = 1
+		_IsSecondColorOn ("Is Second Color On", float) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+			float4 _DefaultColor;
+			float4	_SecondColor;
+			float _IsSecondColorOn;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				if (_IsSecondColorOn == 1) {
+					return _SecondColor;
+				}
+				else {
+					return _DefaultColor;
+				}
+                // sample the texture
+                //fixed4 col = tex2D(_MainTex, i.uv);
+                //return col;
+            }
+            ENDCG
+        }
+    }
+}
