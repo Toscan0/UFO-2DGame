2c9dac4350623b569af8be294d1af974cacff9a8
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-20 03:51:41 +0100

    Events Tutorial

---
+++ Events Tutorial/Assets/Scripts/Cube.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public delegate void ChangeColor(Color newColor);
+    public ChangeColor OnColorChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnColorChange = UpdateColor;
+        OnColorChange(Color.green);
+
+        //Event
+        Main.OnTeleport += ChangePosition;
+        Main.OnTeleport += Aux;
+    }
+
+    public void ChangePosition(Vector3 newPos)
+    {
+        transform.position = newPos;
+    }
+
+    public void Aux(Vector3 newPos)
+    {
+        Debug.Log("Just to show that is possible to agreggate funcs");
+    }
+
+    public void UpdateColor(Color newColor)
+    {
+        GetComponent<MeshRenderer>().material.color = newColor;
+    }
+
+    //Always disable
+    private void OnDisable()
+    {
+        Main.OnTeleport -= ChangePosition;
+        Main.OnTeleport -= Aux;
+    }
+}
---
+++ Events Tutorial/Assets/Scripts/Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public delegate void Teleport(Vector3 pos);
+    public static event Teleport OnTeleport;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(OnTeleport != null)
+            {
+                OnTeleport(new Vector3(0, 2, 0));
+            }
+        }
+    }
+}
