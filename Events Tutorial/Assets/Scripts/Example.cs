c6cd4406586f9b32785c034c46afd00cbf795a7f
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-21 16:57:21 +0100

    Events tutorial finished

---
+++ Events Tutorial/Assets/Scripts/Example.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    //Actions to delegate void functions
+    public Action<int, int> Sum;
+
+    public Action ObjectName;
+
+    // Func to delegate non-void functions
+    // Last parameter is the return type of the func
+    public Func<int> LengthObjectName;
+    public Func<float, float, string> AnotherSum;
+
+
+    void Start()
+    {
+        Sum = (a, b) =>
+        {
+            var total = a + b;
+            if (total < 100)
+            {
+                Debug.Log("Total is less than 100");
+            }
+
+            Debug.Log("Total: " + total);
+        }; //Dont forget ; in the end
+
+        Sum(5, 2);
+
+
+
+        ObjectName = () => Debug.Log("Game Object name: " + gameObject.name);
+        ObjectName();
+
+
+
+        LengthObjectName = () => gameObject.name.Length;
+        int count = LengthObjectName();
+        Debug.Log("Game Object Lenght: " +  count);
+
+
+
+        AnotherSum = (a, b) =>
+        {
+            // with  {} is necessary return if is a non-void func
+            return (a + b).ToString();
+        };
+        string res = AnotherSum(0.2f, 0.4f);
+        Debug.Log("Sum result: " + res);
+    }   
+}
--- Events Tutorial/Assets/Scripts/Main.cs
+++ Events Tutorial/Assets/Scripts/Main.cs
@@ -1,15 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    //Delegate an event
     public delegate void Teleport(Vector3 pos);
     public static event Teleport OnTeleport;
 
+    // Same thing as the two lines above, Action === Event
+    public static Action<Vector3> OnTeleportByAction;
+
+    //Delegate a function
+    public delegate int CharacterLength(string Text);
+
+    // Same thing as the line above
+    public Func<string, int> AnotherCharacterLength;
+
     private void Start()
     {
-        
+        int count = 0;
+
+        CharacterLength cl = GetCharacter;
+        count = cl("John");
+        Debug.Log("Character count: " + count);
+
+        AnotherCharacterLength = GetCharacter;
+        count = AnotherCharacterLength("Mariana");
+        Debug.Log("Character count: " + count);
+
+        // Same thing as above
+        AnotherCharacterLength = (name) => name.Length;
+        count = AnotherCharacterLength("Isabel");
+        Debug.Log("Character count: " + count);
     }
 
     private void Update()
@@ -19,7 +43,13 @@ public class Main : MonoBehaviour
             if(OnTeleport != null)
             {
                 OnTeleport(new Vector3(0, 2, 0));
+                OnTeleportByAction(new Vector3(0, 2, 0));
             }
         }
+    }
+
+    private int GetCharacter(string name)
+    {
+        return name.Length; 
     }
 }
