4653d2b21ae7e89e03b9600d8ce7c8239f19800c
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-21 18:34:38 +0100

    Ring effects examples

---
+++ Particles System/Assets/Misc/Rings/With trail module/ParticleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    public Vector3 speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime);
+    }
+}
