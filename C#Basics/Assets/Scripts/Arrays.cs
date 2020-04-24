1e097928118f9e98eca91ffca291951f78019ee0
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-24 01:56:16 +0100

    Arrays

---
+++ C#Basics/Assets/Scripts/Arrays.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public int itemID;
+    public string name;
+    public string description;
+}
+
+public class Arrays : MonoBehaviour
+{
+    private int[] array1;
+    private int[] array2 = new int[5];
+    private int[] array3 = new int[] { 1, 2, 3, 4, 5 };
+
+    [SerializeField]
+    private Item[] items;
+
+    private void Start()
+    {
+        foreach(var item in items)
+        {
+            Debug.Log(item.itemID);
+        }
+    }
+
+}
