a8fef48b37c529cb50cbd66f87239be8ac55cb67
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-26 00:57:46 +0100

    Dragon Games

--- Dragons Game/Assets/Scripts/Enemy/Enemy.cs
+++ Dragons Game/Assets/Scripts/Enemy/Enemy.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void Die()
     {
-        GetComponent<SpriteRenderer>().enabled = false;
-        GetComponent<Collider2D>().enabled = false;
+        StartCoroutine(MakePlayerDead(0.5f));
+       
     }
 
     public void Respawn()
     {
         GetComponent<SpriteRenderer>().enabled = true;
         GetComponent<Collider2D>().enabled = true;
+    }
+
+    private IEnumerator MakePlayerDead(float waitTime)
+    {
+        animator.SetTrigger("Dead");
+        GetComponent<Collider2D>().enabled = false;
+
+        yield return new WaitForSeconds(waitTime);
+
+        GetComponent<SpriteRenderer>().enabled = false;
     }
 }
