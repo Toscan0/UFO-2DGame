f9d5246bd24db4b44d7c2eea71cda712715f702a
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-26 22:05:10 +0100

    Flappy bird thats actually a fish done

---
+++ Flappy Bird/Assets/Scripts/Fish.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Fish : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce = 100;
+
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+
+        if(transform.position.y > 6 || transform.position.y < -5)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+    [SerializeField]
+    private float randomOffSet = 1.5f;
+
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if(transform.position.x <= -7)
+        {
+            if(randomOffSet == 0)
+            {
+                transform.position = new Vector3(7, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                float randomHeight = UnityEngine.Random.Range(-randomOffSet, randomOffSet);
+                transform.position = new Vector3(7, randomHeight, transform.position.z);
+            }
+        }
+    }
+}
