53d9c819da1264c83b0441df715e66dd3740e244
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-27 17:05:41 +0100

    Part1 done

--- Games/3º Person Shooter/Assets/Scripts/PlayerMovement.cs
+++ Games/3º Person Shooter/Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,18 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 100;
+    [SerializeField]
+    private int turnSpeed = 1;
 
     private Vector3 movement;
 
     private CharacterController characterController;
+    private Animator animator;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -23,6 +27,15 @@ public class PlayerMovement : MonoBehaviour
         var vertical = Input.GetAxis("Vertical");
 
         movement = new Vector3(horizontal, 0, vertical);
+
+        animator.SetFloat("Speed", movement.magnitude);
+
+        if(movement.magnitude > 0)
+        {
+            Quaternion newDirection = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+        }
+        
     }
 
     private void FixedUpdate()
