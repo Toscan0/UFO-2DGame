3f0f3d6b85d7a71d1fd71e588003ba418fc00eb0
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-27 05:16:55 +0100

    Started but is late, tomorrow is day too

---
+++ Games/3º Person Shooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 100;
+
+    private Vector3 movement;
+
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(horizontal, 0, vertical);
+    }
+
+    private void FixedUpdate()
+    {
+        characterController.SimpleMove(movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
