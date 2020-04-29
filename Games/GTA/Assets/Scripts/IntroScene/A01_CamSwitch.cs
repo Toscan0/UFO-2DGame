bdeda60f4c627131bac7fb233746639660b4d0d4
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-29 18:21:19 +0100

    Sky box

---
+++ Games/GTA/Assets/Scripts/IntroScene/A01_CamSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A01_CamSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject firstCam;
+    [SerializeField]
+    private GameObject secondCam;
+
+
+    private void Start()
+    {
+        StartCoroutine(CamSitcher()); 
+    }
+
+    private IEnumerator CamSitcher()
+    {
+        yield return new WaitForSeconds(4);
+        secondCam.SetActive(true);
+        firstCam.SetActive(false);
+    }
+}
