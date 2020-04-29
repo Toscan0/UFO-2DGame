4453da3e45f99900ff092da1374e34b58d78afba
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-29 18:55:01 +0100

    Intro scene start to be nice

---
+++ Games/GTA/Assets/Scripts/IntroScene/A02_MoveChar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A02_MoveChar : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource leftFoot;
+    [SerializeField]
+    private AudioSource rightFoot;
+    [SerializeField]
+    private bool steppingLeft = true;
+    [SerializeField]
+    private GameObject mainChar;
+    [SerializeField]
+    private int stepsTaken;
+
+    private void Start()
+    {
+        StartCoroutine(WalkSequence());
+    }
+
+    private void Update()
+    {
+        mainChar.transform.Translate(0, 0, 0.014f * Time.timeScale);
+    }
+
+    IEnumerator WalkSequence()
+    {
+        yield return new WaitForSeconds(0.4f);
+        while(stepsTaken < 13)
+        {
+            yield return new WaitForSeconds(0.5f);
+            if(steppingLeft == true)
+            {
+                leftFoot.Play();
+                steppingLeft = false;
+            }
+            else
+            {
+                rightFoot.Play();
+                steppingLeft = true;
+            }
+            stepsTaken += 1;
+        }
+        mainChar.SetActive(false);
+    }
+}
