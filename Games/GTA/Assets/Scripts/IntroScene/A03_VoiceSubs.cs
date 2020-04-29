f8c8905fb364672c304cbc588ff0f7a1ddeb8e7d
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-29 21:57:08 +0100

    intro scene done

--- Games/GTA/Assets/Scripts/IntroScene/A03_VoiceSubs.cs
+++ Games/GTA/Assets/Scripts/IntroScene/A03_VoiceSubs.cs
@@ -11,6 +11,22 @@ public class A03_VoiceSubs : MonoBehaviour
     private AudioSource voiceLine01;
     [SerializeField]
     private AudioSource voiceLine02;
+    [SerializeField]
+    private AudioSource voiceLine03;
+    [SerializeField]
+    private AudioSource voiceLine04;
+    [SerializeField]
+    private AudioSource loudBang;
+    [SerializeField]
+    private GameObject fullBlack;
+    [SerializeField]
+    private GameObject deathObj;
+    [SerializeField]
+    private GameObject chairScene;
+    [SerializeField]
+    private AudioSource voiceLine05;
+    [SerializeField]
+    private GameObject fourCam;
 
     private void Start()
     {
@@ -19,6 +35,7 @@ public class A03_VoiceSubs : MonoBehaviour
 
     IEnumerator IntroSub()
     {
+        subText.color = new Color(1, 1, 1);
         yield return new WaitForSeconds(8);
         subText.text = "You asked fot this George.";
         voiceLine01.Play();
@@ -28,8 +45,32 @@ public class A03_VoiceSubs : MonoBehaviour
         yield return new WaitForSeconds(1);
         subText.text = "Lorenzo, I sweat it wasn't me.";
         voiceLine02.Play();
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2.1f);
         subText.text = "";
+        yield return new WaitForSeconds(0.2f);
+        subText.text = "You squeal in horseface, you sleepin' with the fishes.";
+        voiceLine03.Play();
+        yield return new WaitForSeconds(3f);
+        subText.text = "";
+        yield return new WaitForSeconds(0.1f);
+        subText.text = "Lorenzo! Please!";
+        voiceLine04.Play();
+        yield return new WaitForSeconds(0.7f);
+        subText.text = "";
+        yield return new WaitForSeconds(0.3f);
+        loudBang.Play();
+        fullBlack.SetActive(true);
+        deathObj.SetActive(true);
+        chairScene.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        subText.text = "My name is Steve Lorenzo!";
+        voiceLine05.Play();
+        yield return new WaitForSeconds(2f);
+        fullBlack.SetActive(false);
+        fourCam.SetActive(true);
+        subText.text = "Three years ao Jimmy Horseface tries to have me whacked! Set me up";
+        yield return new WaitForSeconds(5f);
+        subText.text = "Now it's time for me to return that favour.";
     }
 
 }
