927a55876157b29868a25c8b11c4d3444b218fa2
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-29 05:44:19 +0100

    Environmet set up

---
+++ Games/GTA/Assets/Scripts/Character/CharacterControler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class CharacterControler : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 150;
+    [SerializeField]
+    private float rootSpeed = 8;
+    [SerializeField]
+    private bool isRunning;
+
+    private float horizontalMove;
+    private float verticalMove;
+
+    private new Animation animation;
+
+    private void Awake()
+    {
+        animation = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            animation.Play("Run");
+            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * rootSpeed;
+            isRunning = true;
+
+            transform.Rotate(0, horizontalMove, 0);
+            transform.Translate(0, 0, verticalMove);
+        }
+        else
+        {
+            animation.Play("Idle");
+            isRunning = false;
+        }
+    }
+}
