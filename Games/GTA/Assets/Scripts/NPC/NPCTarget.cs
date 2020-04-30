806cf95fc3e784c098cf574881351f07c94f1363
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-04-30 17:34:32 +0100

    NPC interaction alf done

--- Games/GTA/Assets/Scripts/NPC/NPCTarget.cs
+++ Games/GTA/Assets/Scripts/NPC/NPCTarget.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 
 public class NPCTarget : MonoBehaviour
 {
+    private int trigNum = 0;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "NPC")
         {
-            this.gameObject.transform.position = new Vector3(91.73f, 0.7f, 150);
+            if (trigNum == 0)
+            {
+                this.gameObject.transform.position = new Vector3(91.73f, 0.7f, 187);
+                trigNum = 1;
+            }
+            if (trigNum == 1)
+            {
+                this.gameObject.transform.position = new Vector3(152f, 0.7f, 188);
+                trigNum = 2;
+            }
+            if (trigNum == 2)
+            {
+                this.gameObject.transform.position = new Vector3(152f, 0.7f, 188);
+                trigNum = 3;
+            }
+            if (trigNum == 3)
+            {
+                this.gameObject.transform.position = new Vector3(152f, 0.7f, 108);
+                trigNum = 4;
+            }
+            if (trigNum == 4)
+            {
+                this.gameObject.transform.position = new Vector3(91.73f, 0.7f, 108); ;
+                trigNum = 0;
+            }
         }
     }
 }
